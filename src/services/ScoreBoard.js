4954b08443a2d344e4d7d88a09048e8482f41254
Mahammad <mahammad.rustamov.2001@gmail.com>
2025-07-01 22:42:55 +0200

    startMatch function im scoreboard implemented

--- src/services/ScoreBoard.js
+++ src/services/ScoreBoard.js
@@ -1,10 +1,14 @@
+const Match = require("../models/Match");
+
 class ScoreBoard {
   constructor() {
     this.matches = [];
   }
 
   startMatch(homeTeam, awayTeam) {
-    // Todo
+    const match = new Match(homeTeam, awayTeam);
+    this.matches.push(match);
+    return this.matches;
   }
 }
 
